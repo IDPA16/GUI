fe9342e9389c90abf6b0aa1b0721c0d867e0b15a
Mafii <mafi@fog.im>
2017-01-07 01:03:10 +0100

    Add basic implementation

--- IDPA16.GUI/Forms/MainWindow.Designer.cs
+++ IDPA16.GUI/Forms/MainWindow.Designer.cs
@@ -29,6 +29,9 @@
     private void InitializeComponent()
     {
       this.lblGithub = new System.Windows.Forms.LinkLabel();
+      this.btnSelectImage = new System.Windows.Forms.Button();
+      this.lblImagePath = new System.Windows.Forms.Label();
+      this.btnStart = new System.Windows.Forms.Button();
       this.SuspendLayout();
       // 
       // lblGithub
@@ -40,12 +43,45 @@
       this.lblGithub.TabIndex = 0;
       this.lblGithub.TabStop = true;
       this.lblGithub.Text = "Github";
+      this.lblGithub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblGithub_LinkClicked);
+      // 
+      // btnSelectImage
+      // 
+      this.btnSelectImage.Location = new System.Drawing.Point(12, 12);
+      this.btnSelectImage.Name = "btnSelectImage";
+      this.btnSelectImage.Size = new System.Drawing.Size(104, 23);
+      this.btnSelectImage.TabIndex = 1;
+      this.btnSelectImage.Text = "Select Image";
+      this.btnSelectImage.UseVisualStyleBackColor = true;
+      this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+      // 
+      // lblImagePath
+      // 
+      this.lblImagePath.AutoSize = true;
+      this.lblImagePath.Location = new System.Drawing.Point(12, 38);
+      this.lblImagePath.Name = "lblImagePath";
+      this.lblImagePath.Size = new System.Drawing.Size(0, 13);
+      this.lblImagePath.TabIndex = 2;
+      // 
+      // btnStart
+      // 
+      this.btnStart.Location = new System.Drawing.Point(12, 54);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(104, 23);
+      this.btnStart.TabIndex = 3;
+      this.btnStart.Text = "Start Classification";
+      this.btnStart.UseVisualStyleBackColor = true;
+      this.btnStart.Visible = false;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
       // 
       // MainWindow
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(369, 247);
+      this.Controls.Add(this.btnStart);
+      this.Controls.Add(this.lblImagePath);
+      this.Controls.Add(this.btnSelectImage);
       this.Controls.Add(this.lblGithub);
       this.Name = "MainWindow";
       this.Text = "IDPA16";
@@ -57,6 +93,9 @@
     #endregion
 
     private System.Windows.Forms.LinkLabel lblGithub;
+    private System.Windows.Forms.Button btnSelectImage;
+    private System.Windows.Forms.Label lblImagePath;
+    private System.Windows.Forms.Button btnStart;
   }
 }
 
--- IDPA16.GUI/Forms/MainWindow.cs
+++ IDPA16.GUI/Forms/MainWindow.cs
@@ -1,18 +1,44 @@
-﻿using System.Windows.Forms;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
 
 namespace IDPA16.GUI.Forms
 {
   public partial class MainWindow : Form
   {
+    private string selectedFilePath;
+
     public MainWindow()
     {
       InitializeComponent();
-      lblGithub.LinkClicked += LblGithub_LinkClicked;
     }
 
-    private void LblGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    private void lblGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
     {
-      System.Diagnostics.Process.Start(@"https://github.com/IDPA16/");
+      Process.Start(@"https://github.com/IDPA16/");
+    }
+
+    private void btnSelectImage_Click(object sender, System.EventArgs e)
+    {
+      var fileDialog = new OpenFileDialog();
+      fileDialog.Multiselect = false;
+      fileDialog.ShowDialog(this);
+      
+      if (File.Exists(fileDialog.FileName))
+      {
+        btnStart.Visible = true;
+        selectedFilePath = fileDialog.FileName;
+        lblImagePath.Text = fileDialog.SafeFileName;
+      }
+      else
+      {
+        lblImagePath.Text = "No file or invalid file selected.";
+      }
+    }
+
+    private void btnStart_Click(object sender, System.EventArgs e)
+    {
+      MessageBox.Show("This will soon start the trained model of hippocrates! Not available yet.");
     }
   }
 }
