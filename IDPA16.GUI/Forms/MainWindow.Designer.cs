681e526d2dca9f013938c881c344a316648e5540
Mafii <mafi@fog.im>
2017-01-07 02:05:48 +0100

    Implement result displaying

--- IDPA16.GUI/Forms/MainWindow.Designer.cs
+++ IDPA16.GUI/Forms/MainWindow.Designer.cs
@@ -32,17 +32,23 @@
       this.btnSelectImage = new System.Windows.Forms.Button();
       this.lblImagePath = new System.Windows.Forms.Label();
       this.btnStart = new System.Windows.Forms.Button();
+      this.lblStatus = new System.Windows.Forms.Label();
+      this.pbxResult = new System.Windows.Forms.PictureBox();
+      this.lblHighestMatch = new System.Windows.Forms.Label();
+      this.lblSecondMatch = new System.Windows.Forms.Label();
+      this.lblWorstMatch = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.pbxResult)).BeginInit();
       this.SuspendLayout();
       // 
       // lblGithub
       // 
       this.lblGithub.AutoSize = true;
-      this.lblGithub.Location = new System.Drawing.Point(319, 9);
+      this.lblGithub.Location = new System.Drawing.Point(536, 12);
       this.lblGithub.Name = "lblGithub";
-      this.lblGithub.Size = new System.Drawing.Size(38, 13);
+      this.lblGithub.Size = new System.Drawing.Size(89, 13);
       this.lblGithub.TabIndex = 0;
       this.lblGithub.TabStop = true;
-      this.lblGithub.Text = "Github";
+      this.lblGithub.Text = "Source @ Github";
       this.lblGithub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblGithub_LinkClicked);
       // 
       // btnSelectImage
@@ -58,33 +64,79 @@
       // lblImagePath
       // 
       this.lblImagePath.AutoSize = true;
-      this.lblImagePath.Location = new System.Drawing.Point(12, 38);
+      this.lblImagePath.Location = new System.Drawing.Point(122, 17);
       this.lblImagePath.Name = "lblImagePath";
       this.lblImagePath.Size = new System.Drawing.Size(0, 13);
       this.lblImagePath.TabIndex = 2;
       // 
       // btnStart
       // 
+      this.btnStart.Enabled = false;
       this.btnStart.Location = new System.Drawing.Point(12, 54);
       this.btnStart.Name = "btnStart";
       this.btnStart.Size = new System.Drawing.Size(104, 23);
       this.btnStart.TabIndex = 3;
       this.btnStart.Text = "Start Classification";
       this.btnStart.UseVisualStyleBackColor = true;
-      this.btnStart.Visible = false;
       this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // lblStatus
+      // 
+      this.lblStatus.AutoSize = true;
+      this.lblStatus.Location = new System.Drawing.Point(122, 59);
+      this.lblStatus.Name = "lblStatus";
+      this.lblStatus.Size = new System.Drawing.Size(0, 13);
+      this.lblStatus.TabIndex = 4;
+      // 
+      // pbxResult
+      // 
+      this.pbxResult.Location = new System.Drawing.Point(12, 83);
+      this.pbxResult.Name = "pbxResult";
+      this.pbxResult.Size = new System.Drawing.Size(333, 352);
+      this.pbxResult.TabIndex = 5;
+      this.pbxResult.TabStop = false;
+      // 
+      // lblHighestMatch
+      // 
+      this.lblHighestMatch.AutoSize = true;
+      this.lblHighestMatch.Location = new System.Drawing.Point(387, 105);
+      this.lblHighestMatch.Name = "lblHighestMatch";
+      this.lblHighestMatch.Size = new System.Drawing.Size(0, 13);
+      this.lblHighestMatch.TabIndex = 6;
+      // 
+      // lblSecondMatch
+      // 
+      this.lblSecondMatch.AutoSize = true;
+      this.lblSecondMatch.Location = new System.Drawing.Point(387, 150);
+      this.lblSecondMatch.Name = "lblSecondMatch";
+      this.lblSecondMatch.Size = new System.Drawing.Size(0, 13);
+      this.lblSecondMatch.TabIndex = 7;
+      // 
+      // lblWorstMatch
+      // 
+      this.lblWorstMatch.AutoSize = true;
+      this.lblWorstMatch.Location = new System.Drawing.Point(387, 174);
+      this.lblWorstMatch.Name = "lblWorstMatch";
+      this.lblWorstMatch.Size = new System.Drawing.Size(0, 13);
+      this.lblWorstMatch.TabIndex = 8;
       // 
       // MainWindow
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(369, 247);
+      this.ClientSize = new System.Drawing.Size(637, 447);
+      this.Controls.Add(this.lblWorstMatch);
+      this.Controls.Add(this.lblSecondMatch);
+      this.Controls.Add(this.lblHighestMatch);
+      this.Controls.Add(this.pbxResult);
+      this.Controls.Add(this.lblStatus);
       this.Controls.Add(this.btnStart);
       this.Controls.Add(this.lblImagePath);
       this.Controls.Add(this.btnSelectImage);
       this.Controls.Add(this.lblGithub);
       this.Name = "MainWindow";
       this.Text = "IDPA16";
+      ((System.ComponentModel.ISupportInitialize)(this.pbxResult)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -96,6 +148,11 @@
     private System.Windows.Forms.Button btnSelectImage;
     private System.Windows.Forms.Label lblImagePath;
     private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Label lblStatus;
+    private System.Windows.Forms.PictureBox pbxResult;
+    private System.Windows.Forms.Label lblHighestMatch;
+    private System.Windows.Forms.Label lblSecondMatch;
+    private System.Windows.Forms.Label lblWorstMatch;
   }
 }
 
--- IDPA16.GUI/Forms/MainWindow.cs
+++ IDPA16.GUI/Forms/MainWindow.cs
@@ -1,6 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
 using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
+using static System.Threading.Thread;
+using static System.Diagnostics.Process;
 
 namespace IDPA16.GUI.Forms
 {
@@ -15,30 +20,96 @@ namespace IDPA16.GUI.Forms
 
     private void lblGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
     {
-      Process.Start(@"https://github.com/IDPA16/");
+      Start(@"https://github.com/IDPA16/");
     }
 
     private void btnSelectImage_Click(object sender, System.EventArgs e)
     {
-      var fileDialog = new OpenFileDialog();
-      fileDialog.Multiselect = false;
+      var fileDialog = new OpenFileDialog { Multiselect = false };
       fileDialog.ShowDialog(this);
-      
+
       if (File.Exists(fileDialog.FileName))
       {
-        btnStart.Visible = true;
+        btnStart.Enabled = true;
         selectedFilePath = fileDialog.FileName;
         lblImagePath.Text = fileDialog.SafeFileName;
+        lblStatus.Text = "Ready";
+        lblStatus.ForeColor = Color.Black;
+        HideResults();
       }
       else
       {
         lblImagePath.Text = "No file or invalid file selected.";
+        HideResults();
       }
     }
 
     private void btnStart_Click(object sender, System.EventArgs e)
     {
+      HideResults();
+      lblStatus.Text = "Running";
       MessageBox.Show("This will soon start the trained model of hippocrates! Not available yet.");
+      Sleep(3000);
+      lblStatus.Text = "Finished";
+      lblStatus.ForeColor = Color.DarkGreen;
+      ShowResults((AnalysisResults.Benign, 97.5m), (AnalysisResults.Normal, 13.25m), (AnalysisResults.Cancer, 5.5m));
+      lblHighestMatch.Font = new Font(this.Font, FontStyle.Underline); ;
+    }
+
+    private void HideResults()
+    {
+      lblHighestMatch.Text = "";
+      lblSecondMatch.Text = "";
+      lblWorstMatch.Text = "";
+    }
+
+    private void ShowResults(
+      (AnalysisResults resultType, decimal percentage) highest,
+      (AnalysisResults resultType, decimal percentage) second,
+      (AnalysisResults resultType, decimal percentage) worst)
+    {
+      pbxResult.BackColor = GetColorForResultType(highest.resultType);
+      SetResultLabel(highest, lblHighestMatch);
+      SetResultLabel(second, lblSecondMatch);
+      SetResultLabel(worst, lblWorstMatch);
+    }
+
+    private Color GetColorForResultType(AnalysisResults type)
+    {
+      switch (type)
+      {
+        case AnalysisResults.Cancer:
+          return Color.Red;
+        case AnalysisResults.Benign:
+          return Color.Yellow;
+        case AnalysisResults.Normal:
+          return Color.Green;
+        default:
+          throw new ArgumentOutOfRangeException(nameof(type), type, null);
+      }
+    }
+
+    private void SetResultLabel((AnalysisResults resultType, decimal percentage) result, Label label)
+    {
+      label.Text = $"{GetEnumDiscription(result.resultType)}: {result.percentage / 100:P}";
+    }
+
+    private static string GetEnumDiscription<T>(T source)
+    {
+      FieldInfo fi = source.GetType().GetField(source.ToString());
+      var attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+      return attributes.Length > 0 ? attributes[0].Description : source.ToString();
+    }
+
+    private enum AnalysisResults
+    {
+      [Description("Cancer")]
+      Cancer,
+      [Description("Benign")]
+      Benign,
+      [Description("Normal")]
+      Normal
     }
   }
 }
